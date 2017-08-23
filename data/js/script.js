83fcbe7033db4942a16b147a56e62652c1ca1fc1
muffin-eater <lucky7s@hellokitty.com>
2017-08-23 19:11:36 +0100

    Added basic input/output functionality.

--- data/js/script.js
+++ data/js/script.js
@@ -1,1 +1,17 @@
 // url-eater script.js //
+
+var inputText = document.getElementById("inputText");
+var modifierText = document.getElementById("modifierText");
+var outputText = document.getElementById("outputText");
+
+var button = document.getElementById("submitBtn");
+if (button.addEventListener)
+	button.addEventListener("click", processUrl, false);
+else if (button.attachEvent)
+	button.attachEvent('onclick', processUrl);
+
+
+function processUrl() {
+	outputText.value = inputText.value + modifierText.value;
+	return;
+}
